711a116fb23f58a4e7d8f4d5ca30c46747d0acbe
rawanazazi12 <rawanazazi12@gmail.com>
2021-07-25 18:36:36 +0300

    solving lab01

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Footer extends React.Component {
+
+    render() {
+        return (
+            <footer>
+
+                <h6>
+                    Author: Rawan Alazazi
+                </h6>
+            </footer>
+        )
+    }
+
+
+}
+
+export default Footer;
---
+++ src/components/HornedBeasts.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class HornedBeasts extends React.Component {
+
+    render() {
+        console.log(this.props);
+        return (
+            <div>
+                <h2> {this.props.title} </h2>
+                <img src={this.props.img} alt= {this.props.title} title={this.props.title}/>
+                <p>{this.props.description} </p>
+            </div>
+        )
+    }
+}
+
+export default HornedBeasts;
