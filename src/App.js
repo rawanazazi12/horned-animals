1a766bc6b23dbe22d5803eb63cbd19c02c3dd0fa
rawanazazi12 <rawanazazi12@gmail.com>
2021-07-27 01:37:52 +0300

    styling the header and footer using bootstrap

--- src/App.js
+++ src/App.js
@@ -21,4 +21,3 @@ class App extends React.Component {
 
 
 export default App;
-
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+
 
 class Footer extends React.Component {
 
   render() {
     return (
-      <footer>
+      
+      [
+        // 'Primary',
+        // 'Secondary',
+        // 'Success',
+        // 'Danger',
+        // 'Warning',
+        // 'Info',
+        // 'Light',
+        'Dark',
+      ].map((variant, idx) => (
+        <Card
+          bg={variant.toLowerCase()}
+          key={idx}
+          text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+          style={{ width: '100%' }}
+          className="mb-2"
+        >
+          <Card.Footer></Card.Footer>
+          <Card.Body>
+            <Card.Title> <h6>Author: Rawan Alazazi</h6> </Card.Title>
+            <Card.Text>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      ))
 
-        <h6>
-            Author: Rawan Alazazi
-        </h6>
-      </footer>
     );
   }
 
